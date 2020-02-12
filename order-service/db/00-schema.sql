aee99cef2a4f99bae56c395d14df7b43ccfa31ac
Adrian Novegil <adrian.novegil@gmail.com>
2020-02-12 22:37:36 +0100

    Updating the SEATA configuration. More tests :-P

--- order-service/db/00-schema.sql
+++ order-service/db/00-schema.sql
@@ -1,28 +1,16 @@
-# Order
-DROP SCHEMA IF EXISTS db_order;
-CREATE SCHEMA db_order;
-USE db_order;
-
-CREATE TABLE `order_tbl`
+-- for AT mode you must to init this sql for you business database. the seata server not need it.
+CREATE TABLE IF NOT EXISTS `undo_log`
 (
-    `id`             INT(11) NOT NULL AUTO_INCREMENT,
-    `user_id`        VARCHAR(255) DEFAULT NULL,
-    `commodity_code` VARCHAR(255) DEFAULT NULL,
-    `count`          INT(11)      DEFAULT '0',
-    `money`          INT(11)      DEFAULT '0',
-    PRIMARY KEY (`id`)
+    `id`            BIGINT(20)   NOT NULL AUTO_INCREMENT COMMENT 'increment id',
+    `branch_id`     BIGINT(20)   NOT NULL COMMENT 'branch transaction id',
+    `xid`           VARCHAR(100) NOT NULL COMMENT 'global transaction id',
+    `context`       VARCHAR(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+    `rollback_info` LONGBLOB     NOT NULL COMMENT 'rollback info',
+    `log_status`    INT(11)      NOT NULL COMMENT '0:normal status,1:defense status',
+    `log_created`   DATETIME     NOT NULL COMMENT 'create datetime',
+    `log_modified`  DATETIME     NOT NULL COMMENT 'modify datetime',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
-CREATE TABLE `undo_log` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `branch_id` bigint(20) NOT NULL,
-  `xid` varchar(100) NOT NULL,
-  `context` varchar(128) NOT NULL,
-  `rollback_info` longblob NOT NULL,
-  `log_status` int(11) NOT NULL,
-  `log_created` datetime NOT NULL,
-  `log_modified` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT ='AT transaction mode undo table';
